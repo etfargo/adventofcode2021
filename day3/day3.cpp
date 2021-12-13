f11e41332ee1b033ad0717b73a941b743eb9d7f1
elliotho <elliot.g.thompson@ndsu.edu>
2021-12-13 16:58:00 -0600

    day3 complete!

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <array>
 
 using namespace std;
 void part1();
@@ -36,6 +37,7 @@ int main(int argc, char *argv[])
         }
         std::printf("argv[1]: %s\n", argv[1]);
     }
+    //part2();
 }
 void part1()
 {
@@ -91,41 +93,102 @@ void part2()
     ifstream file("./day3_input.txt");
     ifstream newFile;
     string inputLine; // 12 binary bit
-    vector<string> input;
-    vector<string> asdf, asdfd;
-    int mostCommonBit[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; 
-    int amount, pos, i = 0;
-    bool flag = false;
+    vector<string> input, tmp, workingset;
+    vector<int> mostCommonBit {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; 
+    int amount, pos, o2, co2;
+    bool flag = true;
+    string o2Gen, co2scrub;
 
     while (file  >> inputLine)
     {
-        for(int i = 0; i < inputLine.length(); i++) {
-            if (inputLine[i] == '1') {
-                mostCommonBit[i]++;
-            } else {
-                mostCommonBit[i]--;
-            }
-        }
         input.push_back(inputLine);
         std::printf("input: %s\n", inputLine.c_str());
     }
-    // consider row[k], bit [i]
-    for(int i = 0; i < 12; i++) {
-        if(flag) {
-            break;
-        }
-        int common = mostCommonBit[i] >= 0 ? 1 : 0;
-        for(int k = 0; k < 1000; k++ ) {
-            std::printf("k: %d, i: %d\n", k, i);
-            // consider only the first bit, find most common (1 if equal)
-            // remove all values that dont
-            //this is bad
-            if ((int)(input.at(k)[i]) != common) {
-                input.at(k) = "";
+    // o2 thing
+    // for each bit if most common bit one, keep only those, else keep only 0
+        // if length is of accepted is 1 end, else repeat
+    int curBit = 0;
+    tmp = input;
+    while(flag) {
+        // --- first update the mostCommonBit for selection (tmp) ---
+        // reset most common bit for tmp
+        mostCommonBit = {0,0,0,0,0,0,0,0,0,0,0,0};
+        // loop through temp
+        for( int i = 0; i < tmp.size(); i++) {
+            // reset bit, then inc/dec each 
+            for(int j = 0; j < tmp[i].length(); j++) {
+                if (tmp[i][j] == '1') {
+                    mostCommonBit[j]++;
+                } else {
+                    mostCommonBit[j]--;
+                }
             }
         }
-        
+        // --- now you can go and do the checking ---
+        int mcb = mostCommonBit[curBit] >= 0 ? 1 : 0;
+        for(int i = 0; i < tmp.size(); i++) {
+            if((tmp[i][curBit] - '0') == mcb ) {
+                workingset.push_back(tmp[i]);
+            }
+        }
+        if(workingset.size() > 1) {
+            tmp = workingset;
+            workingset.clear();
+            // continue
+        } else {
+            flag = false;
+            printf("you 02 gen value is: %s\n", workingset[0].c_str());
+            // get that value!
+            o2Gen = workingset[0];
+        }
+        curBit++;
     }
-    
-    //file.close();
+    // reset for next search
+    flag = true;
+    workingset.clear();
+
+    // c02 thing
+    // for each bit if least common bit one, keep only those, else keep only 0
+        // if length is of accepted is 1 end, else repeat
+    curBit = 0;
+    tmp = input;
+    while(flag) {
+        // --- first update the mostCommonBit for selection (tmp) ---
+        // reset most common bit for tmp
+        mostCommonBit = {0,0,0,0,0,0,0,0,0,0,0,0};
+        // loop through temp
+        for( int i = 0; i < tmp.size(); i++) {
+            // reset bit, then inc/dec wach 
+            for(int j = 0; j < tmp[i].length(); j++) {
+                if (tmp[i][j] == '1') {
+                    mostCommonBit[j]++;
+                } else {
+                    mostCommonBit[j]--;
+                }
+            }
+        }
+        // --- now you can go and do the checking ---
+        int lcb = mostCommonBit[curBit] >= 0 ? 0 : 1;
+        for(int i = 0; i < tmp.size(); i++) {
+            if((tmp[i][curBit] - '0') == lcb ) {
+                workingset.push_back(tmp[i]);
+            }
+        }
+        if(workingset.size() > 1) {
+            tmp = workingset;
+            workingset.clear();
+            // continue
+        } else {
+            flag = false;
+            printf("you c02 value is: %s\n", workingset[0].c_str());
+            // get that value!
+            co2scrub = workingset[0];
+        }
+        curBit++;
+    }
+
+    o2 = std::stoi(o2Gen, nullptr, 2);
+    co2 = std::stoi(co2scrub, nullptr, 2);
+
+    printf("O2 is: %d, CO2 is: %d, Multiplied is: %d\n", o2, co2, o2*co2);
 }
